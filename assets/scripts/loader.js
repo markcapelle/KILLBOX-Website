8bb46ee019b9b4a53beec10cb23b0d5ffb1dbea7
Mark Van Capelle <markcapelle@outlook.ie>
2026-01-13 18:51:20 +0000

    Initial commit

---
+++ assets/scripts/loader.js
@@ -0,0 +1,11 @@
+fetch('assets/banner.html')
+    .then(res => res.text())
+    .then(data => {document.getElementById('Banner').innerHTML = data}); 
+
+fetch('assets/nav.html')
+    .then(res => res.text())
+    .then(data => {document.getElementById('Navigation').innerHTML = data}); 
+
+fetch('assets/footer.html')
+    .then(res => res.text())
+    .then(data => {document.getElementById('Footer').innerHTML = data}); 
