32873f02ed455e268e2861f2ef8ee0384f27eecf
Mark Van Capelle <markcapelle@outlook.ie>
2026-01-13 19:26:31 +0000

    Navbar adjustment
    changed navbar position and CSS so that it runs horizontally above main body for better responsiveness on smaller screens

--- assets/scripts/script.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('scroll', () => {
-//   const nav = document.querySelector('nav');
-//   if (window.scrollY > 50) {
-//     nav.classList.add('scrolled');
-//   } else {
-//     nav.classList.remove('scrolled');
-//   }
-// });
