1cffc53293403e649b3e22ad6c9bf0ecc6a2d52d
Mark Van Capelle <markcapelle@outlook.ie>
2026-01-29 17:18:01 +0000

    sound effects prep
    pseudocode for sound effects control ideas

--- assets/scripts/audio.js
+++ assets/scripts/audio.js
@@ -9,12 +9,19 @@ bgMusic.volume = 1.0;
 // Autoplay on script load
 bgMusic.play();
 
+
+// sound effects parameters
+// boolean as to wether or not to play sound effects
+
+
 audioBtn.addEventListener('click', () => {
     audioEnabled = !audioEnabled; // Toggle state
 
     if (audioEnabled) {
         // Start music
         bgMusic.play();
+
+        // set sound effects boolean to true
 
         // Set audio controls button
         audioBtn.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
@@ -23,7 +30,13 @@ audioBtn.addEventListener('click', () => {
         bgMusic.pause();
         bgMusic.currentTime = 0;
 
+        // set sound effects boolean to false
+
         // Set audio controls button
         audioBtn.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
     }
 });
+
+
+// sound effects controller
+// if sound effects true, allow play of sound effect to be called.
